1745e7a711d2cb8ee4232d3bdaa7616b8684fe89
Mykytyi <40006074+Mykytyi@users.noreply.github.com>
2019-06-13 13:22:03 +0300

    Update scripts/script.js
    Co-Authored-By: Остап П'ятковський <44294945+gestap@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -45,7 +45,7 @@ class Person {
     const today = new Date();
     const birthDate = new Date(dateString);
     let age = today.getFullYear() - birthDate.getFullYear();
-    let month = today.getMonth() - birthDate.getMonth();
+    const month = today.getMonth() - birthDate.getMonth();
     if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
     {
       age--;
